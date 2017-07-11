8dc111579dede4bcaa77bde1d67fa99181b3c1e2
sashan <sashang@gmail.com>
2017-07-11 10:17:06 +1000

    Add a bitset class to make sure I can do it.

--- cracking-the-coding-interview/10-4.cpp
+++ cracking-the-coding-interview/10-4.cpp
@@ -4,6 +4,52 @@
 
 using namespace std;
 
+class Bitset
+{
+    private:
+        int _size;
+        int* _arr;
+
+    public: 
+        Bitset(int size) : _size(size)
+        {
+            int bits_in_int = sizeof(int)*8;
+            _arr = new int[size/bits_in_int];
+        }
+
+        ~Bitset()
+        {
+            delete[] _arr;
+        }
+
+        void clear(int position)
+        {
+            int bits_in_int = sizeof(int)*8;
+            int idx = position/bits_in_int;
+            int offset = position % bits_in_int;
+            int mask = ~(0x1 << offset);
+            _arr[idx] = _arr[idx] & mask;
+        }
+
+        void set(int position)
+        {
+            int bits_in_int = sizeof(int)*8;
+            int idx = position/bits_in_int;
+            int offset = position % bits_in_int;
+            int mask = 0x1 << offset;
+            _arr[idx] = _arr[idx] | mask;
+        }
+
+        bool get(int position) 
+        {
+            int bits_in_int = sizeof(int)*8;
+            int idx = position/bits_in_int;
+            int offset = position % bits_in_int;
+            int mask = 0x1 << offset;
+            return _arr[idx] & mask;
+        }
+};
+
 template <int size> 
 array<int, size> gen_array()
 {
@@ -31,4 +77,11 @@ int main(int argc, char* argv[])
             bs[item] = 1;
         }
     }
+
+    Bitset mybs(10);
+    cout << "pos 4: " << mybs.get(4) << endl;
+    mybs.set(4);
+    cout << "pos 4: " << mybs.get(4) << endl;
+    mybs.clear(4);
+    cout << "pos 4: " << mybs.get(4) << endl;
 }
