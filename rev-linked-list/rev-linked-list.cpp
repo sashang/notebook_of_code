12c040fd14677c913103e77e4a523906d7846b32
sashan <sashang@gmail.com>
2017-11-09 20:56:41 +1100

    Make code more expressive.

--- rev-linked-list/rev-linked-list.cpp
+++ rev-linked-list/rev-linked-list.cpp
@@ -37,17 +37,15 @@ void print_ll(LL* start)
 
 LL* rev_ll(LL* start)
 {
-    LL* cur = start;
-    LL* next_list = cur->next;
-    cur->next = nullptr;
-    while (next_list)
+    LL* newh = start;
+    while (start->next)
     {
-        LL* temp = next_list->next;
-        next_list->next = cur;
-        cur = next_list;
-        next_list = temp;
+        LL* tmp = start->next;
+        start->next = tmp->next;
+        tmp->next = newh;
+        newh = tmp;
     }
-    return cur;
+    return newh;
 }
 
 int main(int argc, char** argv)
