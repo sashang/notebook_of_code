9354f5b9fa88cef0552fbea26b300501e04af906
sashan <sashang@gmail.com>
2017-12-07 20:36:31 +1100

    fast power function

---
+++ powfunction/pow.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+double pow(double x, int n)
+{
+    if (n == 0) return 1;
+    if (n == 1) return x;
+    return x * pow(x, n-1);
+}
+
+double fast_pow(double x, int n)
+{
+    /**
+     * Example: 
+     * 2^0 = 1
+     * 2^1 = 2
+     * 2^2 = 2*2
+     * 2^3 = 2*2^2 = 2*2^1*2^1
+     * 2^4 = 2^2*2^2
+     * 2^5 = 2*2^4 = 2*2^2*2^2
+     * 2^6 = 2^3*2^3
+     * 2^7 = 2*2^6 = 2*2^3*2^3
+     */
+
+    if (n == 0) return 1;
+    if (n == 1) return x;
+    if (n < 0)
+    {
+        n = -n;
+    }
+    if (n % 2 == 0)
+    {
+        double temp = fast_pow(x, n/2);
+        return 1.0/(temp * temp);
+    }
+    else
+    {
+        double temp = fast_pow(x, n/2);
+        return 1.0/(x * temp * temp);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    cout << pow(2,4) << endl;
+    cout << fast_pow(2,4) << endl;
+    cout << fast_pow(2,5) << endl;
+    cout << fast_pow(2,-2) << endl;
+    cout << fast_pow(2,-3) << endl;
+}
