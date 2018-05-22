6d692fce05ee8a2968595aaf51b5488668b0c376
sashan <sashang@gmail.com>
2018-05-22 10:58:36 +1000

    Fix this rev linked list function.

--- rev-linked-list/rev-linked-list.cpp
+++ rev-linked-list/rev-linked-list.cpp
@@ -19,7 +19,6 @@ void create_ll()
         l->i = i;
         *cur = l;
         cur = &(l->next);
-        cout << i << endl;
     }
     *cur = nullptr;
 }
@@ -37,15 +36,18 @@ void print_ll(LL* start)
 
 LL* rev_ll(LL* start)
 {
-    LL* newh = start;
-    while (start->next)
+    LL* current = start;
+    LL* prev = nullptr;
+    LL* next = nullptr;
+
+    while (current)
     {
-        LL* tmp = start->next;
-        start->next = tmp->next;
-        tmp->next = newh;
-        newh = tmp;
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
     }
-    return newh;
+    return prev;
 }
 
 int main(int argc, char** argv)
