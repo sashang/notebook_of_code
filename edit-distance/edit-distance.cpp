2ca2d9faca2680f66efc16ad2a50c7944051ca65
sashan <sashang@gmail.com>
2019-06-29 13:58:36 +1000

    Update edit distance.

--- edit-distance/edit-distance.cpp
+++ edit-distance/edit-distance.cpp
@@ -56,6 +56,21 @@ int min_dist(string word1, string word2) {
     return arr[word1.size()-1][word2.size()-1];
 }
 
+int min_dist_recurse(string word1, int idx1, string word2, int idx2)
+{
+    if (idx1 >= word1.size() && idx2 >= word2.size())
+        return 0;
+    else if (idx1 >= word1.size())
+        return word2.size() - idx2;
+    else if (idx2 >= word2.size())
+        return word1.size() - idx1;
+
+    if (word1[idx1] == word2[idx2])
+        return min_dist_recurse(word1, idx1 + 1, word2, idx2 + 1);
+
+    return 1 + std::min(min_dist_recurse(word1, idx1, word2, idx2 + 1), std::min(min_dist_recurse(word1, idx1+1, word2, idx2), min_dist_recurse(word1, idx1+1, word2, idx2+1)));
+}
+
 int main(int argc, char**)
 {
     cout << min_dist("a", "c") << endl;
@@ -65,6 +80,15 @@ int main(int argc, char**)
     cout << min_dist("ab", "ab") << endl;
     cout << min_dist("abc", "abd") << endl;
     cout << min_dist("abz", "abgh") << endl;
+
+    cout << endl << "Using recursive function" << endl;
+    cout << min_dist_recurse("a", 0, "c", 0) << endl;
+    cout << min_dist_recurse("a", 0, "a", 0) << endl;
+    cout << min_dist_recurse("a", 0, "ab", 0) << endl;
+    cout << min_dist_recurse("ac",0, "ab", 0) << endl;
+    cout << min_dist_recurse("ab", 0, "ab", 0) << endl;
+    cout << min_dist_recurse("abc", 0, "abd", 0) << endl;
+    cout << min_dist_recurse("abz", 0, "abgh", 0) << endl;
     return 0;
 }
 
